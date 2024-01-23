66c6a06cf47f75eadf9147404557fbaca363d711
Irene Toca <irene.devops@gmail.com>
2024-01-23 22:39:16 +0000

    02-strings.py

---
+++ Tipos/02-strings.py
@@ -0,0 +1,13 @@
+nombre_curso = "Ultimate Python"
+descripcion_curso = """
+Ultimate Python
+este curso completa todos los detalles
+que necesitas aprender para encontrar 
+un trabao como programador.
+"""
+print(len (nombre_curso))
+print(nombre_curso[0])
+print(nombre_curso[0:8])
+print(nombre_curso[9:])
+print(nombre_curso[:8])
+print(nombre_curso[:])
