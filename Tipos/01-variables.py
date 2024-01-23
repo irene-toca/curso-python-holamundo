a4c82a93a37f94fa9e5d4499a10a91ee95dbc9a7
Irene Toca <irene.devops@gmail.com>
2024-01-23 16:43:45 +0000

    01-variables.py1

--- Tipos/01-variables.py
+++ Tipos/01-variables.py
@@ -1,2 +1,10 @@
 nombre_curso = "Ultimate Python"
-print(nombre_curso)
+nombre1 = "Hola"
+NOMBRE_CURSO = "Mundo"
+NoMbRe_CuRsO = "Chanchito"
+NombreCurso = "Feliz"
+print(nombre_curso, nombre1,NOMBRE_CURSO,NoMbRe_CuRsO)
+alumnos = 5000
+puntaje = 9.9
+publicado = False
+
