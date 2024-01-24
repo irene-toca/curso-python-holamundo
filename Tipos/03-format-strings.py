a4c6e7ef80c6a36865ece713ddb276ad0c587203
Irene Toca <irene.devops@gmail.com>
2024-01-24 21:13:28 +0000

    03_04-for_met_strings.py

---
+++ Tipos/03-format-strings.py
@@ -0,0 +1,4 @@
+nombre = "Irene"
+apellido = "Toca"
+nombre_completo = f"{nombre[0]} {2+5}"
+print(nombre_completo)
---
+++ Tipos/04-metodos-strings.py
@@ -0,0 +1,15 @@
+animal = "  chanCHito feliz  "
+
+# métodos es una función que se encuentra dentro de un objeto
+
+print(animal.upper())
+print(animal.lower())
+print(animal.strip().capitalize())
+print(animal.title())
+print(animal.strip())
+print(animal.lstrip())
+print(animal.rstrip())
+print(animal.find("CH"))
+print(animal.replace("nCH", "j"))
+print("nCH" in animal)
+print("nCH" not in animal)
