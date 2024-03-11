d54e7bbd818b7bda5726d72c9bb3c27cfe981b38
Irene Toca <irene.devops@gmail.com>
2024-03-11 09:13:01 +0000

    02-if.py

---
+++ cotrol-flujo/02-if.py
@@ -0,0 +1,14 @@
+edad = 70
+if edad > 65:
+    print("Puede ver la película con super descuento")
+elif edad > 54:
+    print(" puedes ver la película con descuento")
+elif edad > 17:
+    print("puede ver la palícula")
+
+#else:
+    print("No puedes entrar")    
+#print("Listo") 
+#print("Ve a otro lado")  
+
+print("Listo")     
